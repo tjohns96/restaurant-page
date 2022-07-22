ad2d567ed3d7d6fb593c8462ce6b5791f47eea22
Trevor Johnson <trevorjohnson9601@gmail.com>
2022-07-22 19:35:31 -0400

    Create the basic layout of the page and the project structure

--- src/home.js
+++ src/home.js
@@ -1,3 +1,5 @@
+import '../node_modules/normalize.css/normalize.css';
+import './home-style.css';
 export default function createHomePage() {
     const content = document.querySelector("#content");
 }
