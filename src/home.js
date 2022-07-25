07c5a28c3a010d30f38e5f464dc3893a6c2eba8c
Trevor Johnson <trevorjohnson9601@gmail.com>
2022-07-25 12:48:50 -0400

    Create home page

--- src/home.js
+++ src/home.js
@@ -2,4 +2,27 @@ import '../node_modules/normalize.css/normalize.css';
 import './home-style.css';
 export default function createHomePage() {
     const content = document.querySelector("#content");
+    const title = document.createElement('p');
+    title.classList.add('title');
+    title.textContent = 'The Gator Garden';
+    const welcomeBlurb = document.createElement('div');
+    welcomeBlurb.classList.add('welcome');
+    welcomeBlurb.textContent = "Welcome to The Gator Garden, home of South Florida's best country bites since 1982. Enjoy juicy burgers, fresh and high-quality seafood, or some of our signature gator plates! Nothing tastes like home like some good gator. Come into our swamp and have a good time!"
+    const gatorImg = document.createElement('img');
+    gatorImg.src = '../src/crocodile2_larger.png';
+    const gatorImg2 = gatorImg.cloneNode(true);
+    gatorImg.classList.add('bottom-deco');
+    gatorImg2.classList.add('top-deco');
+    welcomeBlurb.append(gatorImg, gatorImg2);
+    let hours = document.createElement('div');
+    hours.classList.add('hours');
+    hours.textContent = 'Hours: ';
+    let sundayHours = document.createElement('p');
+    let weekHours = document.createElement('p');
+    let saturdayHours = document.createElement('p');
+    sundayHours.textContent = 'Sunday / Closed';
+    weekHours.textContent = 'Monday-Friday / 11 a.m. - 10 p.m.';
+    saturdayHours.textContent = 'Saturday / 12 p.m. - 8 p.m.';
+    hours.append(sundayHours, weekHours, saturdayHours);
+    content.append(title, welcomeBlurb, hours);
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import createHomePage from './home.js';
 import '../node_modules/normalize.css/normalize.css';
 import './style.css';
+const tabs = document.querySelectorAll('.nav-tab');
+tabs.forEach((tab) => tab.addEventListener('click', () =>{
+    let selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    tab.classList.add('selected');
+}));
+createHomePage();
